a2e77192c66787fa50e3dba00a9bdd141ac640bd
NoCreed <52470087+NoCreed@users.noreply.github.com>
2023-09-29 16:35:04 +0200

    testing

--- extension.js
+++ extension.js
@@ -1,36 +1,69 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { default: axios } = require('axios');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+    const disposable = vscode.commands.registerCommand('extension.startChatGPT', () => {
+        // Create and show a webview to interact with ChatGPT
+        const panel = vscode.window.createWebviewPanel(
+            'chatGPT',
+            'ChatGPT',
+            vscode.ViewColumn.One,
+            {}
+        );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chatgptsupport" is now active!');
+        // Handle user input and send requests to ChatGPT
+        panel.webview.onDidReceiveMessage((message) => {
+            if (message.command === 'sendChatMessage') {
+                // Send user message to ChatGPT API
+                axios.post('CHATGPT_API_URL', { text: message.text })
+                    .then((response) => {
+                        // Display ChatGPT's response in the webview
+                        panel.webview.postMessage({ command: 'updateChat', text: response.data.text });
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            }
+        });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chatgptsupport.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+        // Load your HTML file for the webview (containing the chat interface)
+        panel.webview.html = getWebviewContent();
+    });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ChatGptSupport!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function getWebviewContent() {
+    // Generate HTML content dynamically
+    const htmlContent = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>ChatGPT Webview</title>
+        </head>
+        <body>
+            <h1>Welcome to ChatGPT</h1>
+            <div id="chat-container">
+                <!-- Chat messages will be displayed here -->
+            </div>
+            <input type="text" id="user-input" placeholder="Type your message...">
+            <button id="send-button">Send</button>
+            <script>
+                // JavaScript code to handle chat interactions
+            </script>
+        </body>
+        </html>
+    `;
+
+    return htmlContent;
+}
+
+
+
+// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};

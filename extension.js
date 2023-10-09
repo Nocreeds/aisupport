6fe737de50dea769405e0b5c9bb4a90c2930c16d
NoCreed <52470087+NoCreed@users.noreply.github.com>
2023-10-09 14:41:20 +0200

    Webview

--- extension.js
+++ extension.js
@@ -1,64 +1,86 @@
 const vscode = require('vscode');
 const { default: axios } = require('axios');
-
+const OpenAi = require('openai');
 function activate(context) {
-    const disposable = vscode.commands.registerCommand('extension.startChatGPT', () => {
-        // Create and show a webview to interact with ChatGPT
-        const panel = vscode.window.createWebviewPanel(
-            'chatGPT',
-            'ChatGPT',
-            vscode.ViewColumn.One,
-            {}
-        );
+    const config = vscode.workspace.getConfiguration('chatgpt');
+    const provider = new OpenAiViewProvider(context.extensionUri);
+    provider.initopenai(config.get("apikey"),config.get("organization"))
 
-        // Handle user input and send requests to ChatGPT
-        panel.webview.onDidReceiveMessage((message) => {
-            if (message.command === 'sendChatMessage') {
-                // Send user message to ChatGPT API
-                axios.post('CHATGPT_API_URL', { text: message.text })
-                    .then((response) => {
-                        // Display ChatGPT's response in the webview
-                        panel.webview.postMessage({ command: 'updateChat', text: response.data.text });
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                    });
+    context.subscriptions.push();
+}
+
+class OpenAiViewProvider{
+
+    constructor(_extensionUri) {
+        this._extensionUri = _extensionUri;
+        this.selectedInsideCodeblock = false;
+        this.pasteOnClick = true;
+        this.keepConversation = true;
+        this.timeoutLength = 60;
+    }
+
+    initopenai (apiKey, organization){
+        const openai = new OpenAi.OpenAI({
+        apiKey: apiKey,
+        organization:organization
+    })}
+
+    resolveWebviewView(webviewView, context, _token) {
+        this._view = webviewView;
+        // set options for the webview
+        webviewView.webview.options = {
+            // Allow scripts in the webview
+            enableScripts: true,
+            localResourceRoots: [
+                this._extensionUri
+            ]
+        };
+        // set the HTML for the webview
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+        // add an event listener for messages received by the webview
+/*         webviewView.webview.onDidReceiveMessage(data => {
+            switch (data.type) {
+                case 'codeSelected':
+                    {
+                        // do nothing if the pasteOnClick option is disabled
+                        if (!this.pasteOnClick) {
+                            break;
+                        }
+                        let code = data.value;
+                        code = code.replace(/([^\\])(\$)([^{0-9])/g, "$1\\$$$3");
+                        // insert the code as a snippet into the active text editor
+                        vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(code));
+                        break;
+                    }
+                case 'prompt':
+                    {
+                        //this.search(data.value);
+                    }
             }
-        });
+        }); */
+    }
 
-        // Load your HTML file for the webview (containing the chat interface)
-        panel.webview.html = getWebviewContent();
-    });
+    _getHtmlForWebview(webview) {
+        return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<style>
+				.code {
+					white-space : pre;
+				</style>
+			</head>
+			<body>
+				<input class="h-10 w-full text-white bg-stone-700 p-4 text-sm" type="text" id="prompt-input" />
 
-    context.subscriptions.push(disposable);
+				<div id="response" class="pt-6 text-sm">
+				</div>
+			</body>
+			</html>`;
+    }
+
 }
-
-function getWebviewContent() {
-    // Generate HTML content dynamically
-    const htmlContent = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>ChatGPT Webview</title>
-        </head>
-        <body>
-            <h1>Welcome to ChatGPT</h1>
-            <div id="chat-container">
-                <!-- Chat messages will be displayed here -->
-            </div>
-            <input type="text" id="user-input" placeholder="Type your message...">
-            <button id="send-button">Send</button>
-            <script>
-                // JavaScript code to handle chat interactions
-            </script>
-        </body>
-        </html>
-    `;
-
-    return htmlContent;
-}
-
-
 
 // this method is called when your extension is deactivated
 function deactivate() {}
